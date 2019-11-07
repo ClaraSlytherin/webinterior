dbf7c54cb55c43feefec57d8566df702d5d4bd07
yogaibnu <yogaibnudinataa@gmail.com>
2019-11-07 09:42:58 +0700

    create controller designer + edit view designer

--- application/views/public/designer/index.php
+++ application/views/public/designer/index.php
@@ -1,1 +1,61 @@
-<h2> DESAINER </h2>
+
+    <div class="site-blocks-cover overlay inner-page" style="background-image: url(images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-10">
+            <!-- <span class="sub-text">Desainer Kami</span> -->
+            <h1>Desainer</h1>
+          </div>
+        </div>
+      </div>
+    </div>  
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 mb-4 project-entry">
+            <a href="#" class="d-block figure">
+              <img src="images/img_1.jpg" alt="Image" class="img-fluid">
+            </a>
+            <h3 class="mb-0"><a href="#">Nama Desainer 1</a></h3>
+            <span class="text-muted">Penjelasan</span>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-4 project-entry">
+            <a href="#" class="d-block figure">
+              <img src="images/img_2.jpg" alt="Image" class="img-fluid">
+            </a>
+            <h3 class="mb-0"><a href="#">Nama Desainer 2</a></h3>
+            <span class="text-muted">Penjelasan</span>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-4 project-entry">
+            <a href="#" class="d-block figure">
+              <img src="images/img_3.jpg" alt="Image" class="img-fluid">
+            </a>
+            <h3 class="mb-0"><a href="#">Nama Desainer 3</a></h3>
+            <span class="text-muted">Penjelasan</span>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4 project-entry">
+            <a href="#" class="d-block figure">
+              <img src="images/img_1.jpg" alt="Image" class="img-fluid">
+            </a>
+            <h3 class="mb-0"><a href="#">Nama Desainer 4</a></h3>
+            <span class="text-muted">Penjelasan</span>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-4 project-entry">
+            <a href="#" class="d-block figure">
+              <img src="images/img_2.jpg" alt="Image" class="img-fluid">
+            </a>
+            <h3 class="mb-0"><a href="#">Nama Desainer 5</a></h3>
+            <span class="text-muted">Penjelasan</span>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-4 project-entry">
+            <a href="#" class="d-block figure">
+              <img src="images/img_3.jpg" alt="Image" class="img-fluid">
+            </a>
+            <h3 class="mb-0"><a href="#">Nama Desainer 6</a></h3>
+            <span class="text-muted">Penjelasan</span>
+          </div>
+        </div>
+      </div>
+    </div>
