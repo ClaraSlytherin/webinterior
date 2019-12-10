e1fcecc99f32b3db805a02841236998ae54a305a
yogaibnu <yogaibnudinataa@gmail.com>
2019-12-10 21:26:28 +0700

    change load model

--- application/controllers/Designer.php
+++ application/controllers/Designer.php
@@ -4,13 +4,13 @@ class Designer extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('Item_model');
+        $this->load->model('Designer_model');
     }
 
     public function index($id = '')
     {
         $data['judul'] = 'Desainer';
-        $data['designer'] = $this->Item_model->getNewDesigner();
+        $data['designer'] = $this->Item_model->getJoinItem();
         $this->load->view('public/templates/header', $data);
         $this->load->view('public/designer/index');
         $this->load->view('public/templates/footer');
