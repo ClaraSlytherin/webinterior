8e30b6e0c33b47eea3168c1d4b2fec34c577f5c9
irhamrizaldy <rirham12@gmail.com>
2019-12-10 18:48:26 +0700

    Projects Page

--- application/views/public/templates/header.php
+++ application/views/public/templates/header.php
@@ -2,87 +2,87 @@
 <html lang="en">
 
 <head>
-    <title>Interior Design &mdash; Colorlib Website Template</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title><?= $judul; ?></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900">
-    <link rel="stylesheet" href="assets/front/fonts/icomoon/style.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900">
+  <link rel="stylesheet" href="assets/front/fonts/icomoon/style.css">
 
-    <link rel="stylesheet" href="assets/front/css/magnific-popup.css">
-    <link rel="stylesheet" href="assets/front/css/jquery-ui.css">
-    <link rel="stylesheet" href="assets/front/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="assets/front/css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="assets/front/css/bootstrap-datepicker.css">
-
-
-    <link rel="stylesheet" href="assets/front/fonts/flaticon/font/flaticon.css">
-
-    <link rel="stylesheet" href="assets/front/css/aos.css">
-
-    <link rel="stylesheet" href="assets/front/css/style.css">
-
-    <div class="site-wrap">
-
-<div class="site-mobile-menu">
-  <div class="site-mobile-menu-header">
-    <div class="site-mobile-menu-close mt-3">
-      <span class="icon-close2 js-menu-toggle"></span>
-    </div>
-  </div>
-  <div class="site-mobile-menu-body"></div>
-</div> <!-- .site-mobile-menu -->
+  <link rel="stylesheet" href="assets/front/css/magnific-popup.css">
+  <link rel="stylesheet" href="assets/front/css/jquery-ui.css">
+  <link rel="stylesheet" href="assets/front/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="assets/front/css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="assets/front/css/bootstrap-datepicker.css">
 
 
-<div class="site-navbar-wrap">
-  <div class="site-navbar-top">
-    <div class="container py-3">
-      <div class="row align-items-center">
-        <div class="col-6">
-          <a href="#" class="p-2 pl-0"><span class="icon-twitter"></span></a>
-          <a href="#" class="p-2 pl-0"><span class="icon-facebook"></span></a>
-          <a href="#" class="p-2 pl-0"><span class="icon-linkedin"></span></a>
-          <a href="#" class="p-2 pl-0"><span class="icon-instagram"></span></a>
+  <link rel="stylesheet" href="assets/front/fonts/flaticon/font/flaticon.css">
+
+  <link rel="stylesheet" href="assets/front/css/aos.css">
+
+  <link rel="stylesheet" href="assets/front/css/style.css">
+
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
         </div>
-        <div class="col-6">
-          <div class="d-flex ml-auto">
-            <a href="#" class="d-flex align-items-center ml-auto mr-4">
-              <span class="icon-phone mr-2"></span>
-              <span class="d-none d-md-inline-block">youremail@domain.com</span>
-            </a>
-            <a href="#" class="d-flex align-items-center">
-              <span class="icon-envelope mr-2"></span>
-              <span class="d-none d-md-inline-block">+1 291 2830 302</span>
-            </a>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> <!-- .site-mobile-menu -->
+
+
+    <div class="site-navbar-wrap">
+      <div class="site-navbar-top">
+        <div class="container py-3">
+          <div class="row align-items-center">
+            <div class="col-6">
+              <a href="#" class="p-2 pl-0"><span class="icon-twitter"></span></a>
+              <a href="#" class="p-2 pl-0"><span class="icon-facebook"></span></a>
+              <a href="#" class="p-2 pl-0"><span class="icon-linkedin"></span></a>
+              <a href="#" class="p-2 pl-0"><span class="icon-instagram"></span></a>
+            </div>
+            <div class="col-6">
+              <div class="d-flex ml-auto">
+                <a href="#" class="d-flex align-items-center ml-auto mr-4">
+                  <span class="icon-phone mr-2"></span>
+                  <span class="d-none d-md-inline-block">youremail@domain.com</span>
+                </a>
+                <a href="#" class="d-flex align-items-center">
+                  <span class="icon-envelope mr-2"></span>
+                  <span class="d-none d-md-inline-block">+1 291 2830 302</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="site-navbar">
+        <div class="container py-1">
+          <div class="row align-items-center">
+            <div class="col-2">
+              <h1 class="mb-0 site-logo"><a href="<?= base_url(); ?>home">Ruang Respons</a></h1>
+            </div>
+            <div class="col-10">
+              <nav class="site-navigation text-right" role="navigation">
+                <div class="container">
+                  <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
+
+                  <ul class="site-menu js-clone-nav d-none d-lg-block">
+                    <li><a href="<?= base_url(); ?>home">Halaman Utama</a></li>
+                    <li><a href="<?= base_url(); ?>projects">Projek</a></li>
+                    <li><a href="<?= base_url(); ?>designer">Desainer</a></li>
+                    <li><a href="<?= base_url(); ?>about">Tentang Kami</a></li>
+                    <li><a href="<?= base_url(); ?>contact">Hubungi Kami</a></li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  <div class="site-navbar">
-    <div class="container py-1">
-      <div class="row align-items-center">
-        <div class="col-2">
-          <h1 class="mb-0 site-logo"><a href="<?= base_url(); ?>home">Ruang Respons</a></h1>
-        </div>
-        <div class="col-10">
-          <nav class="site-navigation text-right" role="navigation">
-            <div class="container">
-              <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
-
-              <ul class="site-menu js-clone-nav d-none d-lg-block">
-                <li><a href="<?= base_url(); ?>home">Halaman Utama</a></li>
-                <li><a href="<?= base_url(); ?>projects">Projek</a></li>
-                <li><a href="<?= base_url(); ?>designer">Desainer</a></li>
-                <li><a href="<?= base_url(); ?>about">Tentang Kami</a></li>
-                <li><a href="<?= base_url(); ?>contact">Hubungi Kami</a></li>
-              </ul>
-            </div>
-          </nav>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
 
 </head>
